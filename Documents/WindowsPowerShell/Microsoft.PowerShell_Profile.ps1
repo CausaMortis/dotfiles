3fe9e8b422e7027e1538c10efe8bb4cc346726c7
Jordy Schreuders <3071062+99linesofcode@users.noreply.github.com>
2019-06-13 16:45:12 +0200

    Import ChocolateyProfilee is it exists

--- Documents/WindowsPowerShell/Microsoft.PowerShell_Profile.ps1
+++ Documents/WindowsPowerShell/Microsoft.PowerShell_Profile.ps1
@@ -1,3 +1,9 @@
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
 Set-PSReadlineOption -BellStyle None
+
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
